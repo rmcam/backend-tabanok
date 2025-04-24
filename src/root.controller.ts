e1b9ac8ea66092129cdb3d82420e168cb6e854b5
rmcam <rmcam.dev@gmail.com>
2025-04-24 12:53:46 -0500

    Jwt verify errors

--- src/root.controller.ts
+++ src/root.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 export class RootController {
+  @Public()
   @Get()
   root() {
     return { status: 'ok', message: 'Tabanok API' };
   }
 
+  @Public()
   @Get('healthz')
   healthz() {
     return { status: 'ok', message: 'Tabanok API' };
