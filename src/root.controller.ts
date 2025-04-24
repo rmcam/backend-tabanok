3fe161510e2519d5e5d5f16cbb2a3ccf098087d0
rmcam <rmcam.dev@gmail.com>
2025-04-24 12:08:50 -0500

    Fix root main.ts

--- src/root.controller.ts
+++ src/root.controller.ts
@@ -2,6 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class RootController {
+  @Get()
+  root() {
+    return { status: 'ok', message: 'Tabanok API' };
+  }
+
   @Get('healthz')
   healthz() {
     return { status: 'ok', message: 'Tabanok API' };
