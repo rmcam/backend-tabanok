8b0138bfd5a6eece6f0d833442e57c55e694a2de
rmcam <rmcam.dev@gmail.com>
2025-05-08 20:30:15 -0500

    Dockerfile modified s

---
+++ bootstrap.js
@@ -0,0 +1,11 @@
+if (typeof crypto === 'undefined' || typeof crypto.randomUUID !== 'function') {
+  global.crypto = {
+    randomUUID: () => {
+      // Implementación simple de randomUUID si no existe
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    }
+  };
+}
