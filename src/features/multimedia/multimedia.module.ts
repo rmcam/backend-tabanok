34b48069f9a6933c3fea94846e412b3e4089acbc
rmcam <rmcam.dev@gmail.com>
2025-05-28 09:42:06 -0500

    Set conection with aws s3

--- src/features/multimedia/multimedia.module.ts
+++ src/features/multimedia/multimedia.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'; // Importar ConfigModule
 import { Multimedia } from './entities/multimedia.entity';
 import { MultimediaController } from './multimedia.controller';
 import { MultimediaService } from './multimedia.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Multimedia])],
+  imports: [
+    TypeOrmModule.forFeature([Multimedia]),
+    ConfigModule, // Importar ConfigModule
+  ],
   controllers: [MultimediaController],
   providers: [MultimediaService],
   exports: [TypeOrmModule.forFeature([Multimedia]), MultimediaService], // Exportar MultimediaService
